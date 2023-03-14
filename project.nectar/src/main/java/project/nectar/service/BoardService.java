db25569c613e59c44e2d992c647e2a3fd2be689e
CRE-A <deerl2@naver.com>
2023-03-15 05:40:43 +0900

    update

--- project.nectar/src/main/java/project/nectar/service/BoardService.java
+++ project.nectar/src/main/java/project/nectar/service/BoardService.java
@@ -1,24 +1,24 @@
-package project.nectar.service;
-
-import project.nectar.domain.BoardDto;
-
-import java.util.HashMap;
-import java.util.List;
-
-public interface BoardService {
-    int count() throws Exception;
-
-    List<BoardDto> selectAll() throws Exception;
-
-    List<BoardDto> selectPage(HashMap map) throws Exception;
-
-    BoardDto select(int bno) throws Exception;
-
-    int insert(BoardDto boardDto) throws Exception;
-
-    int deleteAll() throws Exception;
-
-    int delete(HashMap map) throws Exception;
-
-    int update(BoardDto boardDto) throws Exception;
-}
+//package project.nectar.service;
+//
+//import project.nectar.domain.BoardDto;
+//
+//import java.util.HashMap;
+//import java.util.List;
+//
+//public interface BoardService {
+//    int count() throws Exception;
+//
+//    List<BoardDto> selectAll() throws Exception;
+//
+//    List<BoardDto> selectPage(HashMap map) throws Exception;
+//
+//    BoardDto select(int bno) throws Exception;
+//
+//    int insert(BoardDto boardDto) throws Exception;
+//
+//    int deleteAll() throws Exception;
+//
+//    int delete(HashMap map) throws Exception;
+//
+//    int update(BoardDto boardDto) throws Exception;
+//}
--- project.nectar/src/test/java/project/nectar/repository/BoardDaoImpl__Test.java
+++ project.nectar/src/test/java/project/nectar/repository/BoardDaoImpl__Test.java
@@ -1,40 +1,40 @@
-package project.nectar.repository;
-
-import project.nectar.domain.BoardDto;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.*;
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
-
-public class BoardDaoImpl__Test {
-    @Autowired
-    private BoardDao boardDao;
-
-    @Test
-    public void insert_Data() throws Exception {
-        boardDao.deleteAll();
-        assertTrue(boardDao.count()==0);
-
-        for(int i=1; i<=300; i++){
-        BoardDto boardDto = new BoardDto("title "+i,"content "+i, "writer "+i);
-        boardDao.insert(boardDto);
-        }
-    }
-
-    @Test
-    public void insert_Data2() throws Exception {
-//        boardDao_JH.deleteAll();
-//        assertTrue(boardDao_JH.count()==300);
-
-        for(int i=1; i<=20; i++){
-            BoardDto boardDto_JH = new BoardDto("CRUD TEST"+i,"content "+i, "asdf");
-            boardDao.insert(boardDto_JH);
-        }
-    }
-
-}
+//package project.nectar.repository;
+//
+//import project.nectar.domain.BoardDto;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import static org.junit.Assert.*;
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+//
+//public class BoardDaoImpl__Test {
+//    @Autowired
+//    private BoardDao boardDao;
+//
+//    @Test
+//    public void insert_Data() throws Exception {
+//        boardDao.deleteAll();
+//        assertTrue(boardDao.count()==0);
+//
+//        for(int i=1; i<=300; i++){
+//        BoardDto boardDto = new BoardDto("title "+i,"content "+i, "writer "+i);
+//        boardDao.insert(boardDto);
+//        }
+//    }
+//
+//    @Test
+//    public void insert_Data2() throws Exception {
+////        boardDao_JH.deleteAll();
+////        assertTrue(boardDao_JH.count()==300);
+//
+//        for(int i=1; i<=20; i++){
+//            BoardDto boardDto_JH = new BoardDto("CRUD TEST"+i,"content "+i, "asdf");
+//            boardDao.insert(boardDto_JH);
+//        }
+//    }
+//
+//}
